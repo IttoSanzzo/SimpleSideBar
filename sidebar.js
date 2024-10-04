46472ec988618efbe10564f6ff032df206011471
IttoSanzzo <konohittos@gmail.com>
2024-10-04 11:03:58 -0300

    SideBar Done!

---
+++ sidebar.js
@@ -0,0 +1,39 @@
+const root = document.documentElement;
+const searchBar = document.getElementById("searchBarInput");
+const sideBarMiscsSpans = document.querySelectorAll("#sideBarMiscs button span");
+const sideBarButton = document.getElementById("sideBarButton");
+const logoImage = document.getElementById("logoImage")
+const profileInfo = document.getElementById("profileInfo")
+console.log("logged");
+
+function openSideBar() {
+	console.log("opening");
+	root.style.setProperty("--sidebar-width", "250px");
+	root.style.setProperty("--searchbar-color", "#8ed7c6");
+	searchBar.style.setProperty("padding-left", "32px");
+	searchBar.style.setProperty("color", "#084236");
+	searchBar.placeholder = "Buscar";
+	setTimeout(() => {
+		sideBarMiscsSpans.forEach(span => {
+			span.style.display = "inline";
+		});
+		logoImage.style.display = "inline";
+		profileInfo.style.display = "flex";
+	}, 200);
+	sideBarButton.onclick = closeSideBar;
+}
+
+function closeSideBar() {
+	console.log("closing");
+	root.style.setProperty("--sidebar-width", "74px");
+	root.style.setProperty("--searchbar-color", "#dde4e4");
+	searchBar.style.removeProperty("padding-left");
+	searchBar.style.setProperty("color", "transparent");
+	searchBar.placeholder = "";
+	sideBarMiscsSpans.forEach(span => {
+		span.style.display = "none";
+	});
+	logoImage.style.display = "none";
+	profileInfo.style.display = "none";
+	sideBarButton.onclick = openSideBar;
+}
